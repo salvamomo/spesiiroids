cb793a0c4554cecb5e79d7a0f4ab32627db072e3
Salvador Molina <salva.momo@gmail.com>
2019-05-12 01:38:26 +0200

    Added powerups assets, and generate powerUp in random positions every X seconds

--- Main.gd
+++ Main.gd
@@ -1,11 +1,10 @@
 extends Node
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var screen_size
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	screen_size = get_viewport().get_visible_rect().size
 	var enemy =	preload("res://Enemy.tscn")
 	
 	var enemy1 = enemy.instance()
@@ -25,3 +24,19 @@ func _ready():
 
 func _process(delta):
 	pass
+
+
+func _on_PowerUpTimer_timeout():
+	# @todo: Randomize type of power up.
+	var powerUp = preload("res://PowerUp.tscn")
+	var random = RandomNumberGenerator.new()
+	random.randomize()
+	
+	powerUp = powerUp.instance()
+	powerUp.position = Vector2(
+		lerp(screen_size.x * 0.1, screen_size.x * 0.9, random.randf()),
+		lerp(screen_size.y * 0.1, screen_size.y * 0.9, random.randf())
+	)
+	
+	.add_child(powerUp)
+	
---
+++ PowerUp.gd
@@ -0,0 +1,7 @@
+extends Area2D
+
+const MIN_RESPAWN_TIME = 6000
+const MAX_RESPAWN_TIME = 12000
+
+func _ready():
+	pass # Replace with function body.
