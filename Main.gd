b5c151e8ed12e452e91e4e983051169613dbdea0
Salvador Molina <salva.momo@gmail.com>
2019-05-18 13:41:50 +0200

    Notes for implementing chiquito and teresiica power ups

--- Main.gd
+++ Main.gd
@@ -27,13 +27,27 @@ func _process(delta):
 		_spawn_test_enemies()
 
 func _on_Player_powerup_activated(powerup):
+	# HUD Updates.
 	.get_node("HUD/acquired_powerups/powerup_" + (powerup.TYPE + 1) as String).hide()
 	.get_node("HUD/powerup_animation/powerup_" + (powerup.TYPE + 1) as String).show()
 
+	# @todo: If powerUp is TerESIIca, it has a special effect.
+	#   1.- Drop an instance of it for enemies to chase.
+	#       - Drop it at the player position. And get THAT position. (this can be done in the powerup itself).
+	#       - Pass the position to this main function.
+	#         
+	#   2.- Get the EnemySpawner (or Manager) to control enemy movement, until the
+	#       expired signal is triggered.
+	#		- Call a `fixEnemiesDirection` function.
+	#       - On each enemy process function, check for that fixed position and use it, if present.
+	#       - upon expiration, just remove that fixed direction.
+	# @todo: For Chiquito, it is similar. Make enemies bounce on it.
+	#   1.- When activating:
+	#       - Call a fixEnemyMinDistanceToPlayer() on the manager that sets the distance given by the powerup.
+	#       - Make enemies use that distance when processing their movement.
 
 func _on_Player_powerup_acquired(powerup):
 	.get_node("HUD/acquired_powerups/powerup_" + (powerup.TYPE + 1) as String).show()
 
 func _on_PowerUp_effects_expired(powerup):
-	print("MAIN: Power up faded.")
 	.get_node("HUD/powerup_animation/powerup_" + (powerup.TYPE + 1) as String).hide()
--- PowerUp_Teresiica.gd
+++ PowerUp_Teresiica.gd
@@ -2,8 +2,15 @@ extends "res://PowerUp.gd"
 
 const TYPE = 3
 
+func _ready():
+	# Connect this to the game, and emit a custom signal
+	pass
+
 func grant_bonus_to_player(player):
-	print("teresiica + ")
+#	var Enemies = get_tree().get_root().get_node("Main")
+#	self.connect("powerup_effects_expired", Main, "_on_PowerUp_effects_expired")
+#	get_tree().get_root().get_node("Main")
+	pass
 	
 func remove_bonus_from_player(player):
 	print("teresiica -")
