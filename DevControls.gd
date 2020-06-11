928bb50d69bedce98d7f7f7d5e151d023992e802
Salvador Molina <salva.momo@gmail.com>
2020-06-12 00:26:27 +0200

    Add handlers for powerup acquisition in DevTools

--- DevControls.gd
+++ DevControls.gd
@@ -3,6 +3,13 @@ extends Control
 var Main
 var Player
 
+const powerUps = [
+	preload("res://PowerUp_Chiquito.tscn"),
+	preload("res://PowerUp_Vicentin.tscn"),
+	preload("res://PowerUp_MrT.tscn"),
+	preload("res://PowerUp_Teresiica.tscn")
+]
+
 func _ready():
 	Main = get_tree().get_root().get_node("Main")
 	Player = Main.get_node("Player")
@@ -11,9 +18,29 @@ func _process(delta):
 	if Input.is_action_just_pressed("ToggleDevControls"):
 		self.visible = !self.visible
 
-
 func _on_CheckButton_toggled(button_pressed):	
 	if (button_pressed):
 		Player.activate_bouncing_shield()
 	else:
 		Player.deactivate_bouncing_shield()
+
+func _on_GenerateChiquito_pressed():
+	var powerUp = powerUps[0].instance()
+	.add_child(powerUp)
+	Player.add_power_up(powerUp)
+
+
+func _on_GenerateVicentin_pressed():
+	var powerUp = powerUps[1].instance()
+	.add_child(powerUp)
+	Player.add_power_up(powerUp)
+
+func _on_GenerateMrT_pressed():
+	var powerUp = powerUps[2].instance()
+	.add_child(powerUp)
+	Player.add_power_up(powerUp)
+
+func _on_GenerateTeresiica_pressed():
+	var powerUp = powerUps[3].instance()
+	.add_child(powerUp)
+	Player.add_power_up(powerUp)
