6625c5f3abe9d6de01f130c1fb39703076a80140
Salvador Molina <salva.momo@gmail.com>
2022-05-17 11:01:02 +0200

    Allow initial music state to stopped

--- SoundManager.gd
+++ SoundManager.gd
@@ -3,7 +3,7 @@ extends Node
 var MUSIC_SHOULD_PLAY = false
 
 func _ready():
-	MUSIC_SHOULD_PLAY = true
+	MUSIC_SHOULD_PLAY = $BackgroundMusic.is_autoplay_enabled()
 
 func _process(delta):
 	if (!MUSIC_SHOULD_PLAY and _is_music_playing()):
@@ -14,11 +14,20 @@ func _toggle_music(force_toggle = false):
 		return
 
 	var toggle_to_play = _is_music_playing()
+
 	$BackgroundMusic.set_stream_paused(toggle_to_play)
 	MUSIC_SHOULD_PLAY = !toggle_to_play
 
+	# Ensure the stream is actually started.
+	if (MUSIC_SHOULD_PLAY and !$BackgroundMusic.is_playing()):
+		$BackgroundMusic.play()
+
+# Music could be NOT playing if:
+#  1.- The stream is paused.
+#  2.- The stream may not be paused, but it was never started in first place, or
+#      it was stopped at some point.
 func _is_music_playing():
-	return !$BackgroundMusic.get_stream_paused()
+	return !$BackgroundMusic.get_stream_paused() && $BackgroundMusic.is_playing()
 
 func pause_music():
 	if (MUSIC_SHOULD_PLAY):
