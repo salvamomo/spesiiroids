7e976a9ce12e10ee4ca4f3ca3810fdbff6a8f706
Salvador Molina <salva.momo@gmail.com>
2022-06-07 00:42:54 +0200

    Make chiquito length equal to the audio joke, or minimum of 3 secs

--- PowerUp_Chiquito.gd
+++ PowerUp_Chiquito.gd
@@ -2,7 +2,13 @@ extends "res://PowerUp.gd"
 
 const TYPE = 0
 const NAME = "Chiquito"
-	
+
+var default_duration
+
+func _ready():
+	default_duration = duracion
+	._ready()
+
 func grant_bonus_to_player(player):
 	player.activate_bouncing_shield()
 	
@@ -13,4 +19,8 @@ func play_sound_effect():
 	var effect_to_play = randi() % 20 + 1
 	var audio_sfx = load('res://assets/audio/effects/chiquito/chiquito' + str(effect_to_play) + '.wav')
 	$AudioStreamPlayer.set_stream(audio_sfx)
+
+	var audio_duration = $AudioStreamPlayer.get_stream().get_length()
+	duracion = max(default_duration, audio_duration)
+
 	$AudioStreamPlayer.play()
