6147f8ca9a2d18d5c40bfec25c221c040d27593d
Salvador Molina <salva.momo@gmail.com>
2022-06-14 23:56:47 +0200

    Tween life counter and small life acquired icons

--- HUD.gd
+++ HUD.gd
@@ -18,14 +18,30 @@ func _process(_delta):
 	$top_bar/Score.text = "Score: " + LevelManager.score as String
 
 func update_lives():
+	var scale_to_vector = Vector2(1.2, 1.2)
+
 	if Player.lives > 4:
 		$bottom/ship_life_plus/total_lives.text = "x" + Player.lives as String
 		$bottom/ship_life_plus/total_lives.show()
+
+		# Scale up numeric counter when losing / getting a life.
+		$bottom/scale_effect.interpolate_property($bottom/ship_life_plus/total_lives, "rect_scale", Vector2(1.0, 1.0), scale_to_vector, 0.1, Tween.TRANS_LINEAR)
+		$bottom/scale_effect.interpolate_property($bottom/ship_life_plus/total_lives, "rect_scale", scale_to_vector, Vector2(1.0, 1.0), 0.2, Tween.TRANS_LINEAR, Tween.EASE_OUT, 0.1)
+		$bottom/scale_effect.start()
 	elif Player.lives <= 4:
 		$bottom/ship_life_plus/total_lives.hide()
+
+		var current_life_node = null
 		for i in range(1,5):
 			if Player.lives >= i:
-				.get_node("bottom/ship_life_" + i as String).show()
+				current_life_node = .get_node("bottom/ship_life_" + i as String)
+
+				# Scale up life icon when making it visible.
+				if (!current_life_node.visible):
+					current_life_node.show()
+					$bottom/scale_effect.interpolate_property(current_life_node, "scale", Vector2(1.0, 1.0), scale_to_vector, 0.1, Tween.TRANS_LINEAR)
+					$bottom/scale_effect.interpolate_property(current_life_node, "scale", scale_to_vector, Vector2(1.0, 1.0), 0.1, Tween.TRANS_LINEAR, Tween.EASE_OUT, 0.1)
+					$bottom/scale_effect.start()
 			else:
 				.get_node("bottom/ship_life_" + i as String).hide()
 
