38ef41b346660015cce1cb3c42b265a100a1f650
Salvador Molina <salva.momo@gmail.com>
2022-06-19 20:03:18 +0200

    Added credits section

---
+++ game_screens/Credits.gd
@@ -0,0 +1,60 @@
+extends Node
+
+var current_page_index: int = 0
+var pages: Array
+
+func _ready():
+	var stars_velocity = Vector2(0, 30)
+	pages = [$GameInfo, $AssetCredits_1, $AssetCredits_2, $ThankYouContainer]
+	$PagerPrev.hide()
+	$PagerContainer/Pager.text = "1 / " + pages.size() as String
+	$Stars/stars_1.velocity = stars_velocity
+	$Stars/stars_2.velocity = stars_velocity
+	$Stars/stars_3.velocity = stars_velocity
+
+func _process(_delta):
+	if Input.is_action_just_pressed("Exit_Back") or Input.is_action_just_pressed("Start_Pause"):
+		self.queue_free()
+		# warning-ignore:return_value_discarded
+		get_tree().change_scene("res://MainMenu.tscn")
+	elif Input.is_action_just_pressed("ui_left"):
+		paginate_prev()
+	elif Input.is_action_just_pressed("ui_right"):
+		paginate_next()
+
+	if (Input.is_action_just_pressed("FullScreen")):
+		OS.window_fullscreen = !OS.window_fullscreen
+
+
+func paginate_prev():
+	if (current_page_index == 0):
+		return
+
+	$PagerNext.show()
+	pages[current_page_index].hide()
+	current_page_index -= 1
+	pages[current_page_index].show()
+	$PagerContainer/Pager.text = (current_page_index + 1) as String + " / " + pages.size() as String
+
+	if (current_page_index == 0):
+		$PagerPrev.hide()
+
+func paginate_next():
+	if (current_page_index >= pages.size() - 1):
+		return
+
+	$PagerPrev.show()
+	pages[current_page_index].hide()
+	current_page_index += 1
+	pages[current_page_index].show()
+	$PagerContainer/Pager.text = (current_page_index + 1) as String + " / " + pages.size() as String
+
+	if (current_page_index == (pages.size() - 1)):
+		$PagerNext.hide()
+
+func _on_PagerArrow_pressed(prev_next: String):
+	if (prev_next == 'prev'):
+		paginate_prev()
+		return
+
+	paginate_next()
---
+++ game_screens/ThankYouPage.gd
@@ -0,0 +1,16 @@
+extends VBoxContainer
+
+var drawn: bool = false
+
+func _process(_delta):
+	if (!visible):
+		$FadeInTweener.stop_all()
+		drawn = false
+
+	if (visible and !drawn):
+		$ThorinAvatar.modulate.a = 0
+		$Sprite.modulate.a = 0
+		$FadeInTweener.interpolate_property($Sprite, "modulate:a", 0, 1, 2, Tween.TRANS_LINEAR, Tween.EASE_IN, 0.3)
+		$FadeInTweener.interpolate_property($ThorinAvatar, "modulate:a", 0, 1, 2, Tween.TRANS_LINEAR, Tween.EASE_IN, 2.3)
+		$FadeInTweener.start()
+		drawn = true
