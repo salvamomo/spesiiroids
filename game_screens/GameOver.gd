1326de6ba598b831b12cd53c0a71b9b874fdbf45
Salvador Molina <salva.momo@gmail.com>
2022-06-22 23:10:08 +0200

    Relocate level manager, sound manager and main

--- game_screens/GameOver.gd
+++ game_screens/GameOver.gd
@@ -4,6 +4,6 @@ func _process(_delta):
 	if (Input.is_action_pressed("Start_Pause")):
 		self.queue_free()
 		# warning-ignore:return_value_discarded
-		get_tree().change_scene("res://Main.tscn")
+		get_tree().change_scene("res://main/Main.tscn")
 	if (Input.is_action_pressed("Exit_Back")):
 		get_tree().quit()
--- game_screens/MainMenu.gd
+++ game_screens/MainMenu.gd
@@ -7,7 +7,7 @@ func _process(_delta):
 	if (Input.is_action_just_pressed("Start_Pause")):
 		self.queue_free()
 		# warning-ignore:return_value_discarded
-		get_tree().change_scene("res://Main.tscn")
+		get_tree().change_scene("res://main/Main.tscn")
 	if (Input.is_action_just_pressed("Exit_Back")):
 		get_tree().quit()
 	if (Input.is_action_just_pressed("FullScreen")):
