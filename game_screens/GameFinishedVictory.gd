8e91a010c2a060c8cf79afae8993440fae32c708
Salvador Molina <salva.momo@gmail.com>
2022-06-22 22:23:01 +0200

    Remove whiteline

--- game_screens/GameFinishedVictory.gd
+++ game_screens/GameFinishedVictory.gd
@@ -3,7 +3,7 @@ extends Node
 func _ready():
 	$ScoreBox/Score.text = Globals.get_final_score() as String
 	$ScoreBox/Hits_Count.text = Globals.get_hits() as String
-		
+
 
 func _process(_delta):
 	if Input.is_action_just_pressed("Exit_Back") or Input.is_action_just_pressed("Start_Pause"):
