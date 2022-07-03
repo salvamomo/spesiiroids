0a8c8420b01c225dfbd3f022c18d69885ac76cd4
Salvador Molina <salva.momo@gmail.com>
2022-07-03 14:49:23 +0200

    Refactor HUD scene using NodePath for UI elements

--- ui/HUD.gd
+++ ui/HUD.gd
@@ -6,6 +6,16 @@ var Player
 var LevelManager
 var time_start = 0
 
+export (NodePath) var total_lives_path
+export (NodePath) var kills_path
+export (NodePath) var score_path
+export (NodePath) var timer_path
+
+onready var total_lives_label: Label = get_node(total_lives_path)
+onready var kills_label: Label = get_node(kills_path)
+onready var score_label: Label = get_node(score_path)
+onready var timer_label: Label = get_node(timer_path)
+
 func _ready():
 	time_start = OS.get_unix_time()
 	var Main = get_tree().get_root().get_node("Main")
@@ -14,22 +24,22 @@ func _ready():
 	update_lives()
 
 func _process(_delta):
-	$top_bar/Kills.text = "Kills: " + LevelManager.kills as String
-	$top_bar/Score.text = "Score: " + LevelManager.score as String
+	kills_label.text = "Kills: " + LevelManager.kills as String
+	score_label.text = "Score: " + LevelManager.score as String
 
 func update_lives():
 	var scale_to_vector = Vector2(1.2, 1.2)
 
 	if Player.lives > 4:
-		$bottom/ship_life_plus/total_lives.text = "x" + Player.lives as String
-		$bottom/ship_life_plus/total_lives.show()
+		total_lives_label.text = "x" + Player.lives as String
+		total_lives_label.show()
 
 		# Scale up numeric counter when losing / getting a life.
-		$bottom/scale_effect.interpolate_property($bottom/ship_life_plus/total_lives, "rect_scale", Vector2(1.0, 1.0), scale_to_vector, 0.1, Tween.TRANS_LINEAR)
-		$bottom/scale_effect.interpolate_property($bottom/ship_life_plus/total_lives, "rect_scale", scale_to_vector, Vector2(1.0, 1.0), 0.2, Tween.TRANS_LINEAR, Tween.EASE_OUT, 0.1)
+		$bottom/scale_effect.interpolate_property(total_lives_label, "rect_scale", Vector2(1.0, 1.0), scale_to_vector, 0.1, Tween.TRANS_LINEAR)
+		$bottom/scale_effect.interpolate_property(total_lives_label, "rect_scale", scale_to_vector, Vector2(1.0, 1.0), 0.2, Tween.TRANS_LINEAR, Tween.EASE_OUT, 0.1)
 		$bottom/scale_effect.start()
 	elif Player.lives <= 4:
-		$bottom/ship_life_plus/total_lives.hide()
+		total_lives_label.hide()
 
 		var current_life_node = null
 		for i in range(1,5):
@@ -49,4 +59,4 @@ func _on_Player_life_acquired():
 	update_lives()
 
 func _on_RefreshTimer_timeout():
-	$top_bar/Timer.text = "Time: " + (OS.get_unix_time() - time_start) as String
+	timer_label.text = "Time: " + (OS.get_unix_time() - time_start) as String
